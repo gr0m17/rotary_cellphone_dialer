b49892acb9b83871622fa5b4c6a388fe8c3dfec6
myke d <49840412+gr0m17@users.noreply.github.com>
2021-01-16 10:33:57 -0600

    Add files via upload
    added hook features. currently dials a 10 digit number, then clears it or clears it if the hook is hung.

--- rotary_cellphone_dialer_001.ino
+++ rotary_cellphone_dialer_001.ino
@@ -1,15 +1,21 @@
 int pulseState;
 
 int lastpulseState;
-
+int dialerPin;
+int hookPin;
+int hookState;
 int PulseCount;
 int dialDigit;
 int cycleCount;
 String dialNumber;
 void setup()
 {
+  dialerPin = 8;
+  hookPin = 10;
   lastpulseState = HIGH;
-  pinMode(8, INPUT);
+  pinMode(dialerPin, INPUT);
+  pinMode(hookPin, INPUT);
+
   Serial.begin(9600);
   dialNumber = String();
 }
@@ -17,63 +23,71 @@ void setup()
 
 void loop()
 {
-  if (dialNumber.length() < 10) {
-    pulseState = digitalRead(8);
-    
-    if (pulseState != lastpulseState) {
-      
-      if (pulseState == HIGH) {
-        
-        Serial.println("ON ------------------");
-        PulseCount = PulseCount + 1;
-        
-        Serial.println("Pulses: ");
-        Serial.println(PulseCount);
+  hookState = digitalRead(hookPin);
+  if (hookState == LOW) {
+//    Serial.println("hook lifted");
+    if (dialNumber.length() < 10) {
+      pulseState = digitalRead(dialerPin);
 
-      }
-      if (pulseState == LOW) {
-        Serial.println("OFF -------------------");
+      if (pulseState != lastpulseState) {
 
-      }
-      delay(50);
-      lastpulseState = pulseState;
-      cycleCount = 0;
+        if (pulseState == HIGH) {
 
-    } else {
-      //    Serial.println("Pulse Cycle:");
-      cycleCount = cycleCount + 1;
-      //    Serial.println(cycleCount);
-      delay(10);
-      
-      
-      if (cycleCount >= 20) {
-        
-        if (PulseCount > 0) {
-          
-          Serial.println("Dialed:");
+          Serial.println("ON ------------------");
+          PulseCount = PulseCount + 1;
+
+          Serial.println("Pulses: ");
           Serial.println(PulseCount);
 
-          if (PulseCount == 10) {
-            Serial.println("pulsecount=10");
-            dialDigit = 0;
-          } else {
-            dialDigit = PulseCount;
+        }
+        if (pulseState == LOW) {
+          Serial.println("OFF -------------------");
+
+        }
+        delay(50);
+        lastpulseState = pulseState;
+        cycleCount = 0;
+
+      } else {
+        //    Serial.println("Pulse Cycle:");
+        cycleCount = cycleCount + 1;
+        //    Serial.println(cycleCount);
+        delay(10);
+
+
+        if (cycleCount >= 20) {
+
+          if (PulseCount > 0) {
+
+            Serial.println("Dialed:");
+            Serial.println(PulseCount);
+
+            if (PulseCount == 10) {
+              Serial.println("pulsecount=10");
+              dialDigit = 0;
+            } else {
+              dialDigit = PulseCount;
+            }
+            dialNumber = dialNumber + dialDigit;
+            Serial.println("dialNumber: ");
+            Serial.println(dialNumber);
+            PulseCount = 0;
           }
-          dialNumber = dialNumber + dialDigit;
-          Serial.println("dialNumber: ");
-          Serial.println(dialNumber);
-          PulseCount = 0;
         }
       }
+    } else {
+
+      Serial.print("Number Recieved! Dialing:");
+      Serial.print(dialNumber);
+      Serial.println(" ");
+      Serial.println("----");
+      delay(2000);
+      Serial.println("clearing dialNumber.");
+      dialNumber = "";
     }
   } else {
-    
-    Serial.print("Number Recieved! Dialing:");
-    Serial.print(dialNumber);
-    Serial.println(" ");
-    Serial.println("----");
-    delay(2000);
+    Serial.println("Hook Down");
     Serial.println("clearing dialNumber.");
-    dialNumber = "";
+      dialNumber = "";
   }
 }
